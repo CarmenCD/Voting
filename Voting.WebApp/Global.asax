7c1c2021e632b9c4d6918a036289e2bdcdeeed08
Carmen Lup <carmen.danatoiu@gmail.com>
2017-08-19 20:04:25 +0300

    Add project files.

---
+++ Voting.WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Voting.WebApp.MvcApplication" Language="C#" %>
---
+++ Voting.WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Voting.WebApp.Startup))]
+namespace Voting.WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
